373c19bafd5d122ea75ccfccdc64e037764dd3c0
Yuxuan Chen <yuxuanchen@stmarksschool.org>
2018-01-07 14:23:39 -0500

    Second Commit

--- A/src/org/usfirst/frc3566/A/OI.java
+++ A/src/org/usfirst/frc3566/A/OI.java
@@ -22,6 +22,8 @@ import org.usfirst.frc3566.A.subsystems.*;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+	//HI
+	
     //// CREATING BUTTONS
     // One type of button is a joystick button which is any button on a joystick.
     // You create one by telling it which joystick it's on and which button
